ee4c4f009684624d29d53beaac2c2143d023251f
RobertoXocop <roberto95117@gmail.com>
2024-03-24 22:22:35 -0600

    Update devsutest_tables.sql

--- devsutest_tables.sql
+++ devsutest_tables.sql
@@ -1,3 +1,7 @@
+-- -----------------------------------------------------
+-- Database "devsutest"
+-- -----------------------------------------------------
+
 CREATE DATABASE devsutest
     WITH
     OWNER = postgres
